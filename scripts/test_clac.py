d6f22769f6542210d249dab9aa7fedf88441a817
wupeng <17327767735@163.com>
2020-06-05 23:24:48 +0800

    1

--- scripts/test_clac.py
+++ scripts/test_clac.py
@@ -18,10 +18,10 @@ class TestClac(unittest.TestCase):
     def tearDownClass(cls) -> None:
         GetDriver().quit_driver()
 
-    @parameterized.expand(read_txt("data.txt"))
+    # @parameterized.expand(read_txt("data.txt"))
     @allure.step(title="计算")
-    def test_add_clac(self, a, b):
-        self.clac.page_add_clac(a, b)
+    def test_add_clac(self):
+        self.clac.page_add_clac(1, 2)
 
 
 if __name__ == '__main__':
