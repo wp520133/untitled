83eb904d582eaa572a7943167d71ce2c55cfa4b6
wupeng <17327767735@163.com>
2020-06-09 21:18:17 +0800

    no message

--- page/page_clac.py
+++ page/page_clac.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.common.by import By
 import page
 from base.base import Base
-
+import allure
 
 class PageClac(Base):
     # 点击数字
     num = 12
 
+    @allure.step(title="点击数字")
     def page_click_num(self, num):
         for n in str(num):
             loc = By.CSS_SELECTOR, "#simple{}".format(n)
@@ -14,14 +15,17 @@ class PageClac(Base):
         # self.base_click(page.clac_num)
 
     # 点击加号
+    @allure.step(title="点击加号")
     def page_click_add(self):
         self.base_click(page.clac_add)
 
     # 点击等号
+    @allure.step(title="点击等号")
     def page_click_eq(self):
         self.base_click(page.clac_eq)
 
     # 获取结果方法
+    @allure.step(title="获取结果")
     def page_click_result(self):
         self.base_click(page.clac_result)
 
